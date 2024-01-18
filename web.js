c2dd3041add81e1e0664147ea0c25266af9987e7
DESKTOP-LKUG246\환정 <2557636@naver.com>
2024-01-19 02:13:49 +0900

    cafe24_Converse

---
+++ web.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 8001;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/build/index.html");
+});
+
+app.listen(PORT,()=>{
+    console.log(` server started on PORT ${PORT}`);
+});
